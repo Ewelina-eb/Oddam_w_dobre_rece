625a6b493b8c3cc2ad1677ceee146dfe76ee2240
Ewelina Bieniek <ewelinaabieniek@gmail.com>
2020-03-25 10:41:08 +0100

    Add normalize

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './scss/main.scss';
+import Normalize from 'react-normalize';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reeeeeeeaaaaaaaaact!
-        </a>
-      </header>
-    </div>
+const App = () => (
+    <>
+      <Normalize />
+    </>
   );
-}
 
 export default App;
