9550eacaa7f701692481e4ce9544ebb58d64b8a5
Ewelina Bieniek <ewelinaabieniek@gmail.com>
2020-03-29 00:31:29 +0100

    Add style and content to HomeSimpleSteps

--- src/components/Home/HomeSimpleSteps.js
+++ src/components/Home/HomeSimpleSteps.js
@@ -1,7 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Step1 from '../../assets/step1.png';
+import Step2 from '../../assets/step2.png';
+import Step3 from '../../assets/step3.png';
+import Step4 from '../../assets/step4.png';
+import Decoration from '../../assets/Decoration.svg';
 
 const HomeSimpleSteps = () => (
     <>
+        <section className="simple-steps">
+            <div className="title-container">
+                <h3>Wystarczą 4 proste kroki</h3>
+                <div className="decoration">
+                    <img src={Decoration} alt=""></img>
+                </div>
+            </div>
+            <div className="steps-container">
+                <div className="step">
+                    <img src={Step1} alt=""></img>
+                    <h4>Wybierz rzeczy</h4>
+                    <div className="step-line">< hr /></div>
+                    <p>ubrania, zabawki,<br />sprzęt i inne</p>
+                </div>
+                <div className="step">
+                    <img src={Step2} alt=""></img>
+                    <h4>Spakuj je</h4>
+                    <div className="step-line">< hr /></div>
+                    <p>skorzystaj z<br />worków na śmieci</p>
+                </div>
+                <div className="step">
+                    <img src={Step3} alt=""></img>
+                    <h4>Zdecyduj komu<br />chcesz pomóc</h4>
+                    <div className="step-line">< hr /></div>
+                    <p>wybierz zaufane<br />miejsce</p>
+                </div>
+                <div className="step">
+                    <img src={Step4} alt=""></img>
+                    <h4>Zamów kuriera</h4>
+                    <div className="step-line">< hr /></div>
+                    <p>kurier przyjedzie<br />w dogodnym terminie</p>
+                </div>
+            </div>
+            <Link className="simple-steps-link" to="/logowanie"><button className="btn">Oddaj <br /> rzeczy</button></Link>
+        </section>
     </>
 );
 
