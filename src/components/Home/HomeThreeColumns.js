1297863948d7ef0d3015616fdb61beec1ffece7c
Ewelina Bieniek <ewelinaabieniek@gmail.com>
2020-03-28 22:10:05 +0100

    Add style and content to HomeThreeColumns

--- src/components/Home/HomeThreeColumns.js
+++ src/components/Home/HomeThreeColumns.js
@@ -2,6 +2,23 @@ import React from 'react';
 
 const HomeThreeColumns = () => (
     <>
+        <section className="three-columns">
+            <div className="col">
+                <h3>10</h3>
+                <h4>ODDANYCH WORKÓW</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+            </div>
+            <div className="col">
+                <h3>5</h3>
+                <h4>WSPARTYCH ORGANIZACJI</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+            </div>
+            <div className="col">
+                <h3>7</h3>
+                <h4>ZORGANIZOWANY ZBIÓREK</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+            </div>
+        </section>
     </>
 );
 
