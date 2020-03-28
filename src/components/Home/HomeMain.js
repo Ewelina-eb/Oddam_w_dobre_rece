83685292f651fe2db6b151793ac423eea60f4be3
Ewelina Bieniek <ewelinaabieniek@gmail.com>
2020-03-29 00:55:01 +0100

    Import Decoration to HomeMain and fix btns

--- src/components/Home/HomeMain.js
+++ src/components/Home/HomeMain.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomeHeader from './HomeHeader';
 import { Link } from 'react-router-dom';
+import Decoration from '../../assets/Decoration.svg';
 
 const HomeMain = () => (
     <>
@@ -11,7 +12,9 @@ const HomeMain = () => (
                 <div className="main-container">
                     <h2>Zacznij pomagać! <br />
                     Oddaj niechciane rzeczy w zaufane ręce</h2>
-                    <div className="main-content-decoration"></div>
+                    <div className="main-content-decoration">
+                        <img src={Decoration} alt=""></img>
+                    </div>
                     <div className="main-content-buttons">
                         <Link className="main-button" to="/logowanie"><button className="btn">Oddaj <br /> rzeczy</button></Link>
                         <Link className="main-button" to="/logowanie"><button className="btn">Zorganizuj <br /> zbiórkę</button></Link>
