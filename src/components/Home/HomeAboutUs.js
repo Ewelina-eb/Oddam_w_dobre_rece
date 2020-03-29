30ef7c18610c47d926eb79553a9c4a45e2d5ebb1
Ewelina Bieniek <ewelinaabieniek@gmail.com>
2020-03-29 16:38:48 +0200

    Add style and content to HomeAboutUs Add normal font-weight to HomeSimpleSteps

--- src/components/Home/HomeAboutUs.js
+++ src/components/Home/HomeAboutUs.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import Decoration from '../../assets/Decoration.svg';
+import Signature from '../../assets/Signature.svg';
 
 const HomeAboutUs = () => (
     <>
+        <section className="about-us" id="about-us">
+            <div className="about-us-content">
+                <h3>O nas</h3>
+                <div className="decoration">
+                    <img src={Decoration} alt=""></img>
+                </div>
+                <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                <div className="signature">
+                    <img src={Signature} alt=""></img>
+                </div>
+            </div>
+            <div className="about-us-image"></div>
+        </section>
     </>
 );
 
--- src/components/Home/HomeHeaderNav.js
+++ src/components/Home/HomeHeaderNav.js
@@ -6,8 +6,8 @@ const HomeHeaderNav = () => (
             <nav className="header-nav">
                 <ul>
                     <li><Link to="/" spy={true} offset={0} smooth={true} duration={500}>Start</Link></li>
-                    <li><Link to="simple-steps" spy={true} offset={0} smooth={true} duration={500}>O co chodzi?</Link></li>
-                    <li><Link to="HomeAboutUs" spy={true} offset={0} smooth={true} duration={500}>O nas</Link></li>
+                    <li><Link to="simple-steps" spy={true} offset={-45} smooth={true} duration={500}>O co chodzi?</Link></li>
+                    <li><Link to="about-us" spy={true} offset={0} smooth={true} duration={500}>O nas</Link></li>
                     <li><Link to="HomeWhoWeHelp" spy={true} offset={0} smooth={true} duration={500}>Fundacje i organizacje</Link></li>
                     <li><Link to="HomeContact" spy={true} offset={0} smooth={true} duration={500}>Kontakt</Link></li>
                 </ul>
