799024a9c8aba338b59462a602563ac42802e8dc
Ewelina Bieniek <ewelinaabieniek@gmail.com>
2020-03-30 12:56:20 +0200

    Add style and content to Nongovernmental and LocalCollections

--- src/components/Home/Organizations/Fundations.js
+++ src/components/Home/Organizations/Fundations.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Fundations = () => (
     <>
-        <section className="fundations">
+        <section className="organizations">
             <div className="organization-desc">
                 <p>W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.</p>
             </div>
---
+++ src/components/Home/Organizations/Nongovernmental.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Nongovernmental = () => (
+    <>
+        <section className="organizations">
+            <div className="organization-desc">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+            </div>
+            <div className="organizations-list">
+                <div className="organization">
+                    <div className="info">
+                        <h4>Organizacja “Lorem Ipsum 1”</h4>
+                        <p>Quis varius quam quisque id diam vel quam elementum pulvinar.</p>
+                    </div>
+                    <div className="details">
+                        <p>Egestas, sed, tempus</p>
+                    </div>
+                </div>
+                < hr />
+                <div className="organization">
+                    <div className="info">
+                        <h4>Organizacja “Lorem Ipsum 2”</h4>
+                        <p>Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.</p>
+                    </div>
+                    <div className="details">
+                        <p>Ut, aliquam, purus, sit, amet</p>
+                    </div>
+                </div>
+                < hr />
+                <div className="organization">
+                    <div className="info">
+                        <h4>Organizacja “Lorem Ipsum 3”</h4>
+                        <p>Scelerisque in dictum non consectetur a erat nam.</p>
+                    </div>
+                    <div className="details">
+                        <p>Mi, quis, hendrerit, dolor</p>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    </>
+)
+
+export default Nongovernmental;
